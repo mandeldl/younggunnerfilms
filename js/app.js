f8f8f8f48be8b74ceaeb11df2b9f376b7a4b5adc
David Mandel <MandelDL@gmail.com>
2015-02-18 17:52:30 -0500

    Successfully manipulated prev-work tabs from top menu, now need to scroll to there

--- js/app.js
+++ js/app.js
@@ -32,8 +32,29 @@ $(document).ready(function() {
     }
   });
 
+  // Navigating to Previous Work
+  $('.prev-work').on('click', 'li > a', function(event) {
+    event.preventDefault();
+    //Get the target tab link from the a href, store in clicktarget
+    var targetTab = $(this).attr('href');
+    var $targetTab = $(""+targetTab+"");
+    // Store accordion tabs ul in accordionTabs
+    var accordionTabs = $('accordion-tabs-minimal');
+    if (!$targetTab.hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $targetTab.closest('.accordion-tabs-minimal')
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+      $targetTab.next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $targetTab.addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+
+  });
+
 // FitVids - targetting .video-wrapper class, parent class of .video has max-width of 560px.
 
   $('.video-wrapper').fitVids();
-  
+
 });
